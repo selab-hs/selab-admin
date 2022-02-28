262b3cbdb38be07b7e73e011ddc026d0581d5a76
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-02-28 23:31:31 +0900

    init

---
+++ src/main/kotlin/kr/ac/hs/selabadmin/SelabAdminApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.hs.selabadmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SelabAdminApplication
+
+fun main(args: Array<String>) {
+    runApplication<SelabAdminApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/hs/selabadmin/SelabAdminApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.hs.selabadmin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SelabAdminApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
